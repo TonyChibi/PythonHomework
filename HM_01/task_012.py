6e303d66168337216471adbe668eec6c0f2359a1
GlazHorsez <to5gogol@gmail.com>
2023-07-26 22:30:00 +0300

    rearranged code to work correctly with binary search

--- HM_01/task_012.py
+++ HM_01/task_012.py
@@ -4,21 +4,29 @@ sum=int(input("imagine two integers \n insert the SUM of two integers: "))
 mult=int(input("now insert the product of their multiplication: "))
 
 
-a=sum/2
+a=sum//2
 b=sum-a
 breakerCount=0
+top=sum
+bottom=a
 while(a*b!=mult):
     if breakerCount==100:
         break
     print("MAGIC")
-    prev=int(a)
     if a*b>mult:
-        a=a-prev/2
+        prev=a
+        a=a-(top-bottom)//2
+        top=prev
+        
     else:
-        a=a+prev/2
+        prev=a
+        a=a+(top-bottom)//2
+        bottom=prev
     b=sum-a
     breakerCount+=1
+    
 if breakerCount==100:
     print("are you shure you count them well?")
 else:
     print(f"is your integers is {int(a)} and {int(b)}?")
+print(breakerCount)
